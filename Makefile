b206805476a9f7865df54bac807777e7c565baa5
Raphael Lekies <raphael.lekies@it4s.eu>
2016-09-04 18:44:35 +0200

    prepare repository for building a debian package

---
+++ Makefile
@@ -0,0 +1,13 @@
+all:
+	@echo "Specify a target:\nmake armv6l\nmake armv7\nmake i386\n"
+armv6l:
+	sudo bash build.sh "armv6l"
+armv7:
+	sudo bash build.sh "armv7"
+i686:
+	sudo bash build.sh "i686"
+amd64:
+	sudo bash build.sh "amd64"
+clean:
+	sudo rm -f *.deb > /dev/null 2>&1
+	sudo rm -rf files/opt/Citrix/ICAClient/lib > /dev/null 2>&1
---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# build some functions
+NUM_CPUS=`nproc`
+echo "###############"
+echo "### Using ${NUM_CPUS} cores"
+SRC_DIR=$(pwd)
+function fix_arch_ctl()
+{
+	sed '/Architecture/d' -i $1
+	test $(arch)x == i686x && echo "Architecture: i386" >> $1
+	test $(arch)x == armv7lx && echo "Architecture: armhf" >> $1
+	test $(arch)x == x86_64x && echo "Architecture: amd64" >> $1
+	sed '$!N; /^\(.*\)\n\1$/!P; D' -i $1
+}
+function dpkg_build()
+{
+	# Calculate package size and update control file before packaging.
+	if [ ! -e "$1" -o ! -e "$1/DEBIAN/control" ]; then exit 1; fi
+	sed '/^Installed-Size/d' -i "$1/DEBIAN/control"
+	size=$(du -s --apparent-size "$1" | awk '{print $1}')
+	echo "Installed-Size: $size" >> "$1/DEBIAN/control"
+	dpkg -b "$1" "$2"
+}
+# build 
+cd UIDialogLib
+#cmake .
+make "-j${NUM_CPUS}"
+# get back to root
+cd ..
+sed '/Package/d' -i "${SRC_DIR}/files/DEBIAN/control"
+sed '/Depends/d' -i "${SRC_DIR}/files/DEBIAN/control"
+echo "Package: ${1}-libuidialog" >> "${SRC_DIR}/files/DEBIAN/control"
+mkdir -p files/opt/Citrix/ICAClient/lib
+cp -ar UIDialogLib/UIDialogLib.so files/opt/Citrix/ICAClient/lib
+#cp -ar Ressources files/opt/IT4S/startpage
+fix_arch_ctl "files/DEBIAN/control"
+dpkg_build files "${1}-libuidialog.deb"
+
