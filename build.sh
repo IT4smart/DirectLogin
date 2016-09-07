0335bf51bcf7556e9e00666026df96e1a24f205d
Raphael Lekies <raphael.lekies@it4s.eu>
2016-09-07 09:28:29 +0200

    correct directory to pack package

--- build.sh
+++ build.sh
@@ -41,20 +41,21 @@ cd ..
 #dpkg_build files "${1}-libuidialog.deb"
 
 # create directory to unpack the debian package 
-mkdir icaclient_tmp
+mkdir -p icaclient_tmp/DEBIAN
 
 # check on which architecture we are running
 if [ $(dpkg --print-architecture) == i386 ] ; then
     # download the package for this architecture
     wget http://it4s.backup.stockdashboard.de/debian/icaclient_13.3.0.344519_i386.deb
     dpkg-deb -x icaclient_13.3.0.344519_i386.deb icaclient_tmp
-    dpkg-deb -e icaclient_13.3.0.344519_i386.deb icaclient_tmp
+    dpkg-deb -e icaclient_13.3.0.344519_i386.deb icaclient_tmp/DEBIAN
     
     # backup the old library
     mv icaclient_tmp/opt/Citrix/ICAClient/lib/UIDialogLib.so icaclient_tmp/opt/Citrix/ICAClient/lib/UIDialogLib.so.bak
     
     # copy newly compiled library
     cp UIDialogLib/UIDialogLib.so icaclient_tmp/opt/Citrix/ICAClient/lib/UIDialogLib.so
+
     dpkg-deb -b icaclient_tmp icaclient_13.3.0.344519_i386.deb
     
 fi
